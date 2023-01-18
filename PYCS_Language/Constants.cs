a26b3c2fd552ef591bffc214f41a770fe6f131c6
PavloYurchyshynn <gamepaha123@gmail.com>
2023-01-18 10:59:01 +0200

    feat: add IfElseHandler

--- PYCS_Language/Constants.cs
+++ PYCS_Language/Constants.cs
@@ -5,7 +5,6 @@
         public const string LET = "let";
         public const string IF = "if";
         public const string ELSE = "else";
-        public const string ELSE_IF = "elseIf";
         public const string CONSOLE = "print";
     }
 }
--- PYCS_Language/Services/TextRecognition/TextParser.cs
+++ PYCS_Language/Services/TextRecognition/TextParser.cs
@@ -4,17 +4,21 @@
     {
         public void ParseText(string[] text)
         {
-            BasicOperationsHandler basicHandler = new BasicOperationsHandler();
             VariablesHandler varsHandler = new VariablesHandler();
+            IfElseOperationHandler ifElseHandler = new IfElseOperationHandler();
             ConsoleHandler consoleHandler = new ConsoleHandler();
 
             foreach (string s in text)
             {
-                if (s.Contains(BasicOperationsConstants.LET))
+                if (s.Contains(BasicOperationsConstants.LET) && s.IndexOf(BasicOperationsConstants.LET) == 0)
                 {
                     varsHandler.VaribleOperation(s);
                 }
-                else if (s.Contains(BasicOperationsConstants.CONSOLE))
+                else if (s.Contains(BasicOperationsConstants.IF) && s.IndexOf(BasicOperationsConstants.IF) == 0)
+                {
+                    ifElseHandler.IfElseOperation(s, varsHandler);
+                }
+                else if (s.Contains(BasicOperationsConstants.CONSOLE) && s.IndexOf(BasicOperationsConstants.CONSOLE) == 0)
                 {
                     consoleHandler.PrintToConsole(s, varsHandler.Variables);
                 }
