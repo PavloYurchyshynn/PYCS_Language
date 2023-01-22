057888a074693eb781c019c522e3c8bf5e363935
PavloYurchyshynn <gamepaha123@gmail.com>
2023-01-22 16:52:32 +0200

    feat: add error handling

--- PYCS_Language/Operations/IfElseOperationHandler.cs
+++ PYCS_Language/Operations/IfElseOperationHandler.cs
@@ -5,19 +5,28 @@
         public void IfElseExecutor(string value, VariablesHandler varsHandler, int stringIndex)
         {
             ConsoleHandler consoleHandler = new ConsoleHandler();
-            value = value.Trim();
 
-            if (value.Contains(OperationsConstants.LET))
+            try
             {
-                varsHandler.VaribleOperation(value, stringIndex);
+                value = value.Trim();
+
+                if (value.Contains(OperationsConstants.LET))
+                {
+                    varsHandler.VaribleOperation(value, stringIndex);
+                }
+                else if (value.Contains(OperationsConstants.CONSOLE))
+                {
+                    consoleHandler.PrintToConsole(value, varsHandler.Variables, stringIndex);
+                }
+                else
+                {
+                    consoleHandler.PrintError(stringIndex);
+                }
             }
-            else if (value.Contains(OperationsConstants.CONSOLE))
-            {
-                consoleHandler.PrintToConsole(value, varsHandler.Variables, stringIndex);
-            }
-            else
+            catch(Exception ex)
             {
                 consoleHandler.PrintError(stringIndex);
+                Console.WriteLine(ex.Message);
             }
         }
         public void IfElseOperation(string str, VariablesHandler varsHandler, int stringIndex)
@@ -25,34 +34,43 @@
             BooleanOperationHandler booleanHandler = new BooleanOperationHandler();
             ConsoleHandler consoleHandler = new ConsoleHandler();
 
-            string ifBoolValue = str.Substring(str.IndexOf("(") + 1, str.IndexOf(")") - str.IndexOf("(") - 1);
-            string ifValue = str.Substring(str.IndexOf("{") + 1, str.IndexOf("}") - str.IndexOf("{") - 1);
-            string elseString = str.Substring(str.IndexOf(OperationsConstants.ELSE) == -1 ? 0 : str.IndexOf("else"));
-            string elseValue = elseString.Substring(elseString.IndexOf("{") + 1, elseString.IndexOf("}") - elseString.IndexOf("{") - 1);
-            str = str.Replace(ifBoolValue, "");
-            str = str.Replace(ifValue, "");
-            str = str.Replace(elseValue, "");
-            List<string> splitString = new List<string>(str.Split(' '));
-
-            if (splitString[0] == OperationsConstants.IF && 
-                splitString[1].StartsWith("(") && 
-                splitString[1].EndsWith(")") &&
-                splitString[2].StartsWith("{") &&
-                splitString[2].EndsWith("}"))
+            try
             {
-                if (booleanHandler.CompareValues(ifBoolValue, varsHandler.Variables))
+                string ifBoolValue = str.Substring(str.IndexOf("(") + 1, str.IndexOf(")") - str.IndexOf("(") - 1);
+                string ifValue = str.Substring(str.IndexOf("{") + 1, str.IndexOf("}") - str.IndexOf("{") - 1);
+                string elseString = str.Substring(str.IndexOf(OperationsConstants.ELSE) == -1 ? 0 : str.IndexOf("else"));
+                string elseValue = elseString.Substring(elseString.IndexOf("{") + 1, elseString.IndexOf("}") - elseString.IndexOf("{") - 1);
+                str = str.Replace(ifBoolValue, "");
+                str = str.Replace(ifValue, "");
+                str = str.Replace(elseValue, "");
+                List<string> splitString = new List<string>(str.Split(' '));
+
+                if (splitString[0] == OperationsConstants.IF &&
+                    splitString[1].StartsWith("(") &&
+                    splitString[1].EndsWith(")") &&
+                    splitString[2].StartsWith("{") &&
+                    splitString[2].EndsWith("}"))
                 {
-                    IfElseExecutor(ifValue, varsHandler, stringIndex);
-                } 
+                    if (booleanHandler.CompareValues(ifBoolValue, varsHandler.Variables))
+                    {
+                        IfElseExecutor(ifValue, varsHandler, stringIndex);
+                    }
+                    else
+                    {
+                        IfElseExecutor(elseValue, varsHandler, stringIndex);
+                    }
+                }
                 else
                 {
-                    IfElseExecutor(elseValue, varsHandler, stringIndex);
+                    consoleHandler.PrintError(stringIndex);
                 }
             }
-            else
+            catch (Exception ex)
             {
                 consoleHandler.PrintError(stringIndex);
+                Console.WriteLine(ex.Message);
             }
+            
         }
     }
 }
--- PYCS_Language/Services/TextRecognition/TextParser.cs
+++ PYCS_Language/Services/TextRecognition/TextParser.cs
@@ -9,21 +9,29 @@
             ConsoleHandler consoleHandler = new ConsoleHandler();
             int stringIndex = 0;
 
-            foreach (string s in text)
+            try
             {
-                stringIndex++;
-                if (s.Contains(OperationsConstants.LET) && s.IndexOf(OperationsConstants.LET) == 0)
+                foreach (string s in text)
                 {
-                    varsHandler.VaribleOperation(s, stringIndex);
+                    stringIndex++;
+                    if (s.Contains(OperationsConstants.LET) && s.IndexOf(OperationsConstants.LET) == 0)
+                    {
+                        varsHandler.VaribleOperation(s, stringIndex);
+                    }
+                    else if (s.Contains(OperationsConstants.IF) && s.IndexOf(OperationsConstants.IF) == 0)
+                    {
+                        ifElseHandler.IfElseOperation(s, varsHandler, stringIndex);
+                    }
+                    else if (s.Contains(OperationsConstants.CONSOLE) && s.IndexOf(OperationsConstants.CONSOLE) == 0)
+                    {
+                        consoleHandler.PrintToConsole(s, varsHandler.Variables, stringIndex);
+                    }
                 }
-                else if (s.Contains(OperationsConstants.IF) && s.IndexOf(OperationsConstants.IF) == 0)
-                {
-                    ifElseHandler.IfElseOperation(s, varsHandler, stringIndex);
-                }
-                else if (s.Contains(OperationsConstants.CONSOLE) && s.IndexOf(OperationsConstants.CONSOLE) == 0)
-                {
-                    consoleHandler.PrintToConsole(s, varsHandler.Variables, stringIndex);
-                }
+            }
+            catch(Exception ex)
+            {
+                consoleHandler.PrintError(stringIndex);
+                Console.WriteLine(ex.Message);
             }
         }
     }
