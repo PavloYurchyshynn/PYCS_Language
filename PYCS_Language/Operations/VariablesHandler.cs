07a758036e66fea40c4d3ff0f6be00dde1fa58b4
PavloYurchyshynn <gamepaha123@gmail.com>
2023-01-25 12:29:16 +0200

    feat: add editVariable method

--- PYCS_Language/Operations/VariablesHandler.cs
+++ PYCS_Language/Operations/VariablesHandler.cs
@@ -3,7 +3,31 @@
     public class VariablesHandler
     {
         public Dictionary<string, double> Variables = new Dictionary<string, double>();
-        public void VaribleOperation(string str, int stringIndex)
+        public void EditVariable(string str, VariablesHandler varsHandler, int stringIndex)
+        {
+            ConsoleHandler consoleHandler = new ConsoleHandler();
+            BasicOperationsHandler basicHandler = new BasicOperationsHandler();
+
+            try
+            {
+                List<string> splitString = new List<string>(str.Split(' '));
+
+                if (varsHandler.Variables.ContainsKey(splitString[0]) && splitString[1] == "=")
+                {
+                    varsHandler.Variables[splitString[0]] = basicHandler.Eval(str.Substring(str.IndexOf("=") + 1), varsHandler.Variables);
+                } 
+                else
+                {
+                    consoleHandler.PrintError(stringIndex);
+                }
+            }
+            catch(Exception ex)
+            {
+                consoleHandler.PrintError(stringIndex);
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void CreateVarible(string str, int stringIndex)
         {
             ConsoleHandler consoleHandler = new ConsoleHandler();
             BasicOperationsHandler basicHandler = new BasicOperationsHandler();
